30a62b9212ede60a3a16579401262d4141f03885
jayne-marceli <jaynemarcely71@gmail.com>
2023-10-06 19:24:05 -0300

    adicionando header

---
+++ recentroJay/src/components/StyledHeader.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const StyledHeader = styled.header`
+  background-color: white;
+  padding: 10px;
+`
+
+export const Navbar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+
+  img {
+    height: 60px;
+  }
+`
