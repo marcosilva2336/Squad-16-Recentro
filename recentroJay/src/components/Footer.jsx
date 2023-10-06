7f4e8a2dcab9546beb39d43d9cb3cfee063d2244
jayne-marceli <jaynemarcely71@gmail.com>
2023-10-06 19:31:11 -0300

    adicionando footer

---
+++ recentroJay/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyledFooter } from './StyledFooter'
+import logoRecentro from '../assets/logorecentro.png'
+
+function Footer() {
+  return (
+    <StyledFooter>
+      <img src={logoRecentro} alt="" />
+    </StyledFooter>
+  )
+}
+
+export default Footer
+
+
+
+
+
+
+
