16a50c0d8991972949c9b2dcfa3a5895def121b6
eliseusalomao <eliseusalomao20@gmail.com>
2023-10-08 08:29:10 -0300

    cria estado para exibir modal

--- Front-end/src/components/header/Header.jsx
+++ Front-end/src/components/header/Header.jsx
@@ -2,38 +2,43 @@ import React, { useState } from 'react'
 
 import { StyledHeader, Navbar } from './StyledHeader'
 
+import Modal from '../modal/Modal'
+
 import recentroLogo from '../../assets/logorecentro.png'
 import prefeituraLogo from '../../assets/logoprefeitura.jpg'
-import Modal from '../modal/Modal'
 
 function Header() {
   const [isModalOpen, setIsModalOpen] = useState(false)
 
-  function handleIsModalOpen() {
+  function handleOpenModal() {
     setIsModalOpen((state) => state ? false : true)
   }
 
   console.log(isModalOpen)
 
   return (
-    <>
-      <StyledHeader>
-        <Navbar>
-          <a href="https://recentro.recife.pe.gov.br/">
-            <img src={recentroLogo} alt="Logo da Recentro" />
+    <StyledHeader>
+      <Navbar>
+        <a href="https://recentro.recife.pe.gov.br/">
+          <img src={recentroLogo} alt="Logo da Recentro" />
+        </a>
+        <div>
+          <button onClick={handleOpenModal}>Login</button>
+          <a href="https://www2.recife.pe.gov.br/">
+            <img src={prefeituraLogo} alt="Logo da Prefeitura" />
           </a>
-          <div>
-            <button onClick={handleIsModalOpen}>Login</button>
-            <a href="https://www2.recife.pe.gov.br/">
-              <img src={prefeituraLogo} alt="Logo da Prefeitura" />
-            </a>
-          </div>
-        </Navbar>
-        {isModalOpen ? <Modal /> : false}
-      </StyledHeader>
-    </>
-
-
+        </div>
+      </Navbar>
+      {isModalOpen && (
+        <Modal onCloseModal={handleOpenModal}>
+          <h1>MODAL</h1>
+          <h1>MODAL</h1>
+          <h1>MODAL</h1>
+          <h1>MODAL</h1>
+          <h1>MODAL</h1>
+        </Modal>
+      )}
+    </StyledHeader>
   )
 }
 
