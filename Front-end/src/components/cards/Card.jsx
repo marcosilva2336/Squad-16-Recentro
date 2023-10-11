e6c12defea607526b0e63f19dd39c231c75d7509
eliseusalomao <eliseusalomao20@gmail.com>
2023-10-11 19:47:10 -0300

    Resolve erro de imagens não sendo renderizadas

--- Front-end/src/components/cards/Card.jsx
+++ Front-end/src/components/cards/Card.jsx
@@ -1,34 +1,35 @@
-import React from "react";
-import { Box, Text, Title, Content, Imagem } from "./StyledCard";
+import React from 'react'
+import { Box, Text, Title, Content, Imagem, CardContainer } from './StyledCard'
+import img from '../../assets/imagem-card-1.jpg'
+import img2 from '../../assets/imagem-card-2.png'
+import img3 from '../../assets/imagem-card-3.png'
 
-
-function Card({img}){
-    return(
-        <>
-            <Box>
-                <Imagem src={img} alt="Imagem 2" />
-                <Content>
-                    <Title>Rua 2</Title>
-                    <Text>Breve descrição para a Rua 2.</Text>
-                </Content>
-            </Box>
-            <Box>
-                <Imagem src={img} alt="Imagem 2" />
-                <Content>
-                    <Title>Rua 2</Title>
-                    <Text>Breve descrição para a Rua 2.</Text>
-                </Content>
-            </Box>
-            <Box>
-                <Imagem src={img} alt="Imagem 2" />
-                <Content>
-                    <Title>Rua 2</Title>
-                    <Text>Breve descrição para a Rua 2.</Text>
-                </Content>
-            </Box>
-            
-        </>
-    )
+function Card(){
+  return(
+    <CardContainer>
+      <Box>
+        <Imagem src={img} alt="Imagem 2" />
+        <Content>
+          <Title>Rua 2</Title>
+          <Text>Breve descrição para a Rua 2.</Text>
+        </Content>
+      </Box>
+      <Box>
+        <Imagem src={img2} alt="Imagem 2" />
+        <Content>
+          <Title>Rua 2</Title>
+          <Text>Breve descrição para a Rua 2.</Text>
+        </Content>
+      </Box>
+      <Box>
+        <Imagem src={img3} alt="Imagem 2" />
+        <Content>
+          <Title>Rua 2</Title>
+          <Text>Breve descrição para a Rua 2.</Text>
+        </Content>
+      </Box>
+    </CardContainer>
+  )
 }
 
 export default Card
--- Front-end/src/pages/Main.jsx
+++ Front-end/src/pages/Main.jsx
@@ -1,7 +1,6 @@
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
 import Banner from '../components/banner/Banner'
-import MapC from '../components/map/Map'
 import BoxMap from '../components/BoxMap/BoxMap'
 import Card from '../components/cards/Card'
 
