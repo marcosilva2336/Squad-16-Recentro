b48cffd7bb9e2d18cafe58136145e3ef283dfd69
João Marcos <114954938+marcosilva2336@users.noreply.github.com>
2023-11-14 20:33:03 -0300

    Tela Atualizar - Responsiva

--- Front-end/src/components/header-atualizar/StyledHeader-atualizar.jsx
+++ Front-end/src/components/header-atualizar/StyledHeader-atualizar.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+
+
 export const Header = styled.div`
   background-color: #fff;
   padding: 25px 0px;
@@ -6,6 +8,8 @@ export const Header = styled.div`
   text-align: center;
   color: white;
   position: relative;
+
+ 
 `;
 
 export const HeaderImage = styled.img`
@@ -14,9 +18,20 @@ export const HeaderImage = styled.img`
   ${props => props.right ? 'right: 10px;' : 'left: 10px;'}
   transform: translateY(-50%);
   width: ${props => props.right ? '150px' : '190px'};
+
+  @media screen and (max-width:768px){
+    width: ${props => props.right ? '120px' : '150px'};
+  }
+  @media screen and (max-width:425px){
+    width: ${props => props.right ? '90px' : '100px'};
+  }
 `;
 
 export const Title = styled.h2`
   text-align: center;
   text-shadow: 1px 1px 5px #000;
+
+  @media screen and (max-width:425px){
+   font-size:15px;
+  }
 `;
--- Front-end/src/components/tabela-atualizar/StyledTabela-atualizar.jsx
+++ Front-end/src/components/tabela-atualizar/StyledTabela-atualizar.jsx
@@ -13,6 +13,23 @@ export const Container = styled.div`
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
   position: relative;
   z-index: 2;
+  
+  @media screen and (max-width:768px){
+    width: 600px;
+    margin: 100px auto 10px;
+  }
+
+  @media screen and (max-width:425px){
+    width: 418px;
+    margin: 100px auto 10px;
+    padding: 20px;
+  }
+
+  @media screen and (max-width:375px){
+    width: 350px;
+    margin: 100px auto 5px;
+    padding: 20px;
+  }
 `;
 
 export const ProgressBar = styled.div`
@@ -23,6 +40,23 @@ export const ProgressBar = styled.div`
   margin-bottom: 30px;
   margin-left: 250px;
   margin-right: 250px;
+
+  @media screen and (max-width:768px){
+    margin-left: 40px;
+    margin-right: 40px;
+    margin-bottom: 50px;
+  }
+  @media screen and (max-width:425px){
+    margin-top: 10px;
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-bottom: 50px;
+  }
+  @media screen and (max-width:375px){
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-bottom: 56px;
+  }
 
   &::after {
     content: '';
@@ -50,7 +84,15 @@ export const Step = styled.div`
   position: relative;
   transition: background-color 0.4s ease;
   z-index: 2;
+  @media screen and (max-width:768px){
+   width: 45px;
+   height: 45px;
+  }
 
+  @media screen and (max-width:375px){
+    width: 40px;
+   height: 40px;
+  }
   &::before {
     content: attr(data-step);
     position: absolute;
@@ -78,6 +120,15 @@ export const FormField = styled.div`
     border: 1px solid #ccc;
     border-radius: 10px;
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
+
+
+  @media screen and (max-width:768px){
+   flex: 0 0 calc(36% - 30px);
+  }
+
+  @media screen and (max-width:375px){
+   flex: 0 0 calc(35% - 10px);
   }
 `;
 
@@ -109,4 +160,11 @@ export const ArrowIcon = styled(FaArrowLeft)`
 
 export const Title2 = styled.h2`
   text-align: center;
+  
+  @media screen and (max-width:768px){
+  margin-bottom: 20px;
+  }
+  @media screen and (max-width:375px){
+  margin-bottom: 30px;
+  }
 `;
